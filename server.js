710b041b3c3fe6349c1f5e256c278c527fa10f29
Davi Emboaba <configuration@daviemboaba.com>
2019-07-04 22:12:02 -0300

    inserindo no banco

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-const express = require('express');
-
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 // Iniciando o app
 const app = express();
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi", 
+    { useNewUrlParser: true }
+);
+requireDir("./src/models");
+
+const Product = mongoose.model("Product");
 
 // Primeira rota
-app.get('/', (req, res) => {
-    res.send('Helo World!');
+app.get("/", (req, res) => {
+    Product.create({ 
+        title: 'React Native', 
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+
+    return res.send("Helo World!");
 });
 
 app.listen(3001);
