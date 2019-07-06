7d0277f7a75fc7c3a853bbf7903c70e99d9b4182
Davi Emboaba <configuration@daviemboaba.com>
2019-07-06 00:41:16 -0300

    usando rotas

--- server.js
+++ server.js
@@ -12,17 +12,7 @@ mongoose.connect(
 );
 requireDir("./src/models");
 
-const Product = mongoose.model("Product");
-
-// Primeira rota
-app.get("/", (req, res) => {
-    Product.create({ 
-        title: 'React Native', 
-        description: 'Build native apps with React',
-        url: 'http://github.com/facebook/react-native'
-    });
-
-    return res.send("Helo World!");
-});
+// Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
