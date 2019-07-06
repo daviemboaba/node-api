027c6c260b829524c2e043cc585b89088cc37c21
Davi Emboaba <configuration@daviemboaba.com>
2019-07-06 16:47:50 -0300

    mongoosePaginate and Cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 // Iniciando o app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Iniciando o DB
 mongoose.connect(
